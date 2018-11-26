3e79ad198c81b926f6d7bee158552257505e0378
Tobias Junghans <tobydox@veyon.io>
2018-11-26 13:49:23 +0100

    Bump version

--- conf.py
+++ conf.py
@@ -59,7 +59,7 @@ author = 'Tobias Junghans'
 # built documents.
 #
 # The short X.Y version.
-version = '4.1.4'
+version = '4.1.5'
 # The full version, including alpha/beta/rc tags.
 release = version
 
