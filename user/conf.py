fc1c1f5776e3d71dbd4640132ede1c619100cf5b
Tobias Junghans <tobydox@veyon.io>
2019-02-22 12:18:45 +0100

    Update copyright

--- user/conf.py
+++ user/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Veyon'
-copyright = '2017-2018, Tobias Junghans'
+copyright = '2017-2019, Veyon Solutions'
 author = 'Tobias Junghans'
 
 # The version info for the project you're documenting, acts as replacement for
