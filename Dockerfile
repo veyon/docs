235dff36279f81d7d994fe3674864e8daec93384
Tobias Junghans <tobydox@veyon.io>
2019-03-22 14:26:28 +0100

    Import build files and CI definitions

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM ubuntu:bionic
+MAINTAINER Tobias Junghans <tobydox@veyon.io>
+
+RUN \
+	apt-get update && \
+	DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+		make python3-sphinx sphinx-intl \
+		fonts-linuxlibertine \
+		gettext \
+		latexmk \
+		texlive-xetex \
+		texlive-latex-recommended \
+		texlive-fonts-recommended \
+		texlive-fonts-extra \
+		texlive-latex-extra \
+		texlive-lang-cyrillic \
+		texlive-lang-european \
+		texlive-lang-german \
+		texlive-lang-italian \
+		texlive-lang-spanish \
+		&& \
+	apt-get clean && \
+	rm -rf /var/lib/apt/lists/*
+
+ENV LC_ALL=C.UTF-8
+ENV LANG=C.UTF-8
+ENV LANGUAGE=C.UTF-8
---
+++ build-manuals.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import subprocess
+import gettext
+
+version = '4.2.0'
+
+builds = [
+	{ 'language': 'de', 'paper': 'a4paper', 'babel': 'ngerman' },
+	{ 'language': 'en', 'paper': 'letterpaper', 'babel': 'USenglish' },
+	{ 'language': 'es', 'paper': 'a4paper', 'babel': 'spanish' },
+	{ 'language': 'it', 'paper': 'a4paper', 'babel': 'italian' },
+	{ 'language': 'sl', 'paper': 'a4paper', 'babel': 'slovene' },
+	{ 'language': 'uk', 'paper': 'a4paper', 'babel': 'ukrainian' },
+]
+
+for manual in [ 'admin', 'user' ]:
+	for i in builds:
+		language = i['language']
+		print( 'Building for language "%s"' % ( language ) )
+		subprocess.Popen( ['msgfmt', 'locale/%s/LC_MESSAGES/%s.po' % ( language, manual ), '-o',
+			'locale/%s/LC_MESSAGES/%s.mo' % ( language, manual ) ] ).wait()
+		env = os.environ.copy()
+		with open('%s/index.rst' % (manual)) as f:
+			title = f.readline().rstrip()
+			title = gettext.translation(manual, 'locale', [language], None, True).gettext(title)
+			env['TITLE'] = title;
+		env['LANGUAGE'] = language
+		env['PAPER'] = i['paper']
+		env['INDEX'] = '%s/index' % ( manual )
+		env['BABEL'] = i['babel']
+		env['VERSION'] = version
+		env['SPHINXOPTS'] = '-j%s' % ( os.cpu_count()+1 )
+		shutil.rmtree('_build', True)
+		subprocess.Popen( ['make', 'latexpdf' ], env=env ).wait()
+		shutil.copyfile('_build/latex/veyon.pdf', 'veyon-%s-manual-%s_%s.pdf' % ( manual, language, version ))
+
